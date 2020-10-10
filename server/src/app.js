d5c1313069be6f873da83a0e80462784be6fa41e
Ирина Лукашевич <luiryna322@gmail.com>
2020-10-11 01:26:53 +0300

    some chages

--- server/src/app.js
+++ server/src/app.js
@@ -1,11 +1,10 @@
 const express = require('express');
 const app = express(); 
-// setting port
-app.set('port',process.env.PORT||3000);
 
-//Middlewares
+app.set('port',process.env.PORT||3001);
+
 app.use(express.json());
-// Cors configuration
+
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
@@ -14,9 +13,8 @@ app.use((req, res, next) => {
     next();
 });
 
-//importing route
 const userRouters = require('./routes/userRoute');
-//Route
+
 app.use('/user', userRouters);
 
 app.use('/', (req,res) => {
